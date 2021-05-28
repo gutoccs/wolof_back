9651d0dff3e40299da074f41d6b10048241f4327
Gustavo Escobar Cobos <gustavo.escobar@outlook.com>
2021-05-27 22:06:22 -0400

    Agregando aquete tymon/jwt-auth. Modificando modelo User para soporte de Token. Agregando excepciones en handler.php sobre el token. AuthController tiene la lógica del login. Agregada rutas en api.php con el prefijo auth. En auth.php se cambió el guard a api y el api a JWT y agregada la configuración de JWT

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        if(User::where('email', $request->input('email'))->count() > 0) {
+
+            $user = User::where('email', $request->input('email'))->first();
+
+            //if($user->flag_login == true) {
+                $credentials = $request->only('email', 'password');
+                if ($token = $this->guard()->attempt($credentials)) {
+                    return response()->json(['status' => 'success'], 200)->header('Authorization', $token);
+                }
+                return response()->json(['error' => 'login_error'], 401);
+            /*}
+            else {
+                return response()->json(['error' => 'login_error Usuario no tiene permiso de autenticarse'], 401);
+            } */
+        }
+        else return response()->json(['error' => 'login_error Usuario no existe'], 401);
+    }
+
+    public function logout()
+    {
+        $this->guard()->logout();
+        return response()->json([
+            'status' => 'success'
+        ], 200);
+    }
+
+    public function user(Request $request)
+    {
+        $user = DB::table('users')
+                ->leftJoin('role_user', 'users.id', '=', 'role_user.user_id');
+
+
+        if(Employee::where('user_id', Auth::user()->id)->first()){
+            $user = $user->leftJoin('employees', 'users.id', '=', 'employees.user_id')
+                        ->select('users.id as id', 'email', 'name', 'surname', 'role_user.role_id as role', 'employees.id as employee_id');
+        }
+        else {
+            $user = $user->leftJoin('clients', 'users.id', '=', 'clients.user_id')
+                        ->select('users.id as id', 'email', 'names as name', 'surnames as surname', 'role_user.role_id as role');
+        }
+
+        $user = $user->where('users.id', '=', Auth::user()->id)->first();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $user
+        ]);
+    }
+
+    public function refresh()
+    {
+        if ($token = $this->guard()->refresh()) {
+            return response()
+                ->json(['status' => 'successs'], 200)
+                ->header('Authorization', $token);
+        }
+        return response()->json(['error' => 'refresh_token_error'], 401);
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function payload()
+    {
+        return response()->json(auth()->payload());
+    }
+
+    private function guard()
+    {
+        return Auth::guard();
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login']);
+    //Route::post('forgot-password', 'ForgotPasswordController');
+    //Route::post('reset-password', 'ResetPasswordController@reset');
+    Route::get('refresh', [AuthController::class, 'refresh']);
+    Route::group(['middleware' => 'auth:api'], function(){
+        Route::get('user', [AuthController::class, 'user']);
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::get('me', [AuthController::class, 'me']);//->middleware('isLevelSevenOrMore');
+        Route::get('payload', [AuthController::class, 'payload']);//->middleware('isLevelSevenOrMore');
+    });
 });
