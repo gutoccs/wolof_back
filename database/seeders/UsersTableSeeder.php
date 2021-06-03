91dde0dc4c65f363bae89baffb5af56109211f2f
Gustavo Escobar Cobos <gustavo.escobar@outlook.com>
2021-06-02 23:10:37 -0400

    Haciendo seeders de usuarios, un usuario por cada tipo del mismo (rol)

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -14,36 +14,119 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $userRole = config('roles.models.role')::where('name', '=', 'User')->first();
-        $adminRole = config('roles.models.role')::where('name', '=', 'Admin')->first();
+
+        $ceoRole = config('roles.models.role')::where('slug', '=', 'ceo')->first();
+        $ctoRole = config('roles.models.role')::where('slug', '=', 'cto')->first();
+        $wolofEmployeeRole = config('roles.models.role')::where('slug', '=', 'wolof.employee')->first();
+
+        $commerceOwnerRole = config('roles.models.role')::where('slug', '=', 'commerce.owner')->first();
+        $commerceEmployeeRole = config('roles.models.role')::where('slug', '=', 'commerce.employee')->first();
+
+        $clientRole = config('roles.models.role')::where('slug', '=', 'client')->first();
+
         $permissions = config('roles.models.permission')::all();
+
+
 
         /*
          * Add Users
          *
          */
-        if (config('roles.models.defaultUser')::where('email', '=', 'admin@admin.com')->first() === null) {
+
+        // System
+        if (config('roles.models.defaultUser')::where('email', '=', 'system@wolof.com')->first() === null) {
             $newUser = config('roles.models.defaultUser')::create([
-                'name'     => 'Admin',
-                'email'    => 'admin@admin.com',
-                'password' => bcrypt('password'),
+                'email'         => 'system@wolof.com',
+                'password'      =>  bcrypt(Str::random(18)),
+                'flag_login'    =>  false,
+                'observation_flag_login'    =>  'El sistema no necesita autenticarse'
             ]);
 
-            $newUser->attachRole($adminRole);
+            $newUser->attachRole($wolofEmployeeRole);
             foreach ($permissions as $permission) {
                 $newUser->attachPermission($permission);
             }
         }
 
-        if (config('roles.models.defaultUser')::where('email', '=', 'user@user.com')->first() === null) {
+        // CEO
+        if (config('roles.models.defaultUser')::where('email', '=', 'jazmin@wolof.com')->first() === null) {
             $newUser = config('roles.models.defaultUser')::create([
-                'name'     => 'User',
-                'email'    => 'user@user.com',
-                'password' => bcrypt('password'),
+                'email'         => 'jazmin@wolof.com',
+                'password'      =>  bcrypt('password'),
+                'flag_login'    =>  true,
+                'observation_flag_login'    =>  null,
+                'username'      =>  'jazmin'
             ]);
 
-            $newUser;
-            $newUser->attachRole($userRole);
+            $newUser->attachRole($ceoRole);
+            foreach ($permissions as $permission) {
+                $newUser->attachPermission($permission);
+            }
         }
+
+        // CTO
+        if (config('roles.models.defaultUser')::where('email', '=', 'gustavo@wolof.com')->first() === null) {
+            $newUser = config('roles.models.defaultUser')::create([
+                'email'         => 'gustavo@wolof.com',
+                'password'      =>  bcrypt('password'),
+                'flag_login'    =>  true,
+                'observation_flag_login'    =>  null,
+                'username'      =>  'gutoccs'
+            ]);
+
+            $newUser->attachRole($ctoRole);
+            foreach ($permissions as $permission) {
+                $newUser->attachPermission($permission);
+            }
+        }
+
+        // Wolof's Employee
+        if (config('roles.models.defaultUser')::where('email', '=', 'employee@wolof.com')->first() === null) {
+            $newUser = config('roles.models.defaultUser')::create([
+                'email'         => 'employee@wolof.com',
+                'password'      =>  bcrypt('password'),
+                'flag_login'    =>  true,
+                'observation_flag_login'    =>  null
+            ]);
+
+            $newUser->attachRole($wolofEmployeeRole);
+        }
+
+        // Commerce Owner
+        if (config('roles.models.defaultUser')::where('email', '=', 'commerceowner@wolof.com')->first() === null) {
+            $newUser = config('roles.models.defaultUser')::create([
+                'email'         => 'commerceowner@wolof.com',
+                'password'      =>  bcrypt('password'),
+                'flag_login'    =>  true,
+                'observation_flag_login'    =>  null
+            ]);
+
+            $newUser->attachRole($commerceOwnerRole);
+        }
+
+        // Commerce Employee
+        if (config('roles.models.defaultUser')::where('email', '=', 'commerceemployee@wolof.com')->first() === null) {
+            $newUser = config('roles.models.defaultUser')::create([
+                'email'         => 'commerceemployee@wolof.com',
+                'password'      =>  bcrypt('password'),
+                'flag_login'    =>  true,
+                'observation_flag_login'    =>  null
+            ]);
+
+            $newUser->attachRole($commerceEmployeeRole);
+        }
+
+        // Client
+        if (config('roles.models.defaultUser')::where('email', '=', 'client@wolof.com')->first() === null) {
+            $newUser = config('roles.models.defaultUser')::create([
+                'email'         => 'client@wolof.com',
+                'password'      =>  bcrypt('password'),
+                'flag_login'    =>  true,
+                'observation_flag_login'    =>  null
+            ]);
+
+            $newUser->attachRole($clientRole);
+        }
+
     }
 }
