b368dc4c465d90e3a7bbe0d51f93c11a94494df0
Gustavo Escobar Cobos <gustavo.escobar@outlook.com>
2021-06-02 23:07:41 -0400

    Agregando los Middlewares chequeo por nivel mínimo del rol y chequeo por tipo de usuario (rol)

---
+++ app/Http/Middleware/CheckMinimumLevel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckMinimumLevel
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, $level)
+    {
+        if(Auth::user()->level() >= $level) {
+            return $next($request);
+        }
+        else {
+            return response()->json(['error' => 'Unauthorized - No tiene nivel suficiente'], 403);
+        }
+    }
+}
