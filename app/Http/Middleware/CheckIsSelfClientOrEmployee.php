1fec9e4be840fd8ae987e529eec802cf94f3d6f9
Gustavo Escobar Cobos <gustavo.escobar@outlook.com>
2021-06-04 02:16:37 -0400

    Creando middleware para dar acceso a un recurso si le pertenece al cliente o el que lo solicita es un empleado

---
+++ app/Http/Middleware/CheckIsSelfClientOrEmployee.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckIsSelfClientOrEmployee
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(isset(Auth::user()->client->id))
+        {
+            if(Auth::user()->client->id == $request->route()->parameter('idClient'))
+                return $next($request);
+        }
+
+        if(Auth::user()->employee)
+            return $next($request);
+
+
+        return response()->json(['error' => 'Unauthorized - Recurso no le pertenece'], 403);
+    }
+}
