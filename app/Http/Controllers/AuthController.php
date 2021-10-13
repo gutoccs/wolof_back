b4529889128cfd4af875d4d02255360a855b6c4e
Gustavo Escobar Cobos <gustavo.escobar@outlook.com>
2021-10-13 02:43:35 -0400

    En los dos logins se envían datos adicionales para guardar en la App. Login por medio de email se iguala al login por teléfono celular

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,11 +7,28 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
+use jeremykenedy\LaravelRoles\Models\Role;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
+        $validator = Validator::make($request->all(),
+        [
+            'email'                 =>  'email|max:255|exists:users,email',
+            'password'              =>  'string|between:8,255'
+        ],
+        [
+            'email.email'           =>  'El Correo Electrónico tiene un formato inválido',
+            'email.max'             =>  'El Correo Electrónico debe tener una longitud máxima de 255 caracteres',
+            'email.exists'          =>  'El Correo Electrónico y/o la Contraseña no coinciden',
+            'password.string'       =>  'La Contraseña contiene caracteres inválidos',
+            'password.between'      =>  'La Contraseña debe contener entre 8 y 255 caracteres',
+        ]);
+
+        if($validator->fails())
+            return response()->json(['errors'   =>  $validator->errors()], 422);
+
         if(User::where('email', $request->input('email'))->count() > 0) {
 
             $user = User::where('email', $request->input('email'))->first();
@@ -19,15 +36,39 @@ class AuthController extends Controller
             if($user->flag_login == true) {
                 $credentials = $request->only('email', 'password');
                 if ($token = $this->guard()->attempt($credentials)) {
-                    return response()->json(['status' => 'success', 'token' => $token], 200)->header('Authorization', $token);
+                    $userType;
+                    $fullName;
+                    if($user->hasRole(['ceo', 'cto', 'gabu.employee'])) {
+                        $userType = 'employee';
+                        $fullName = $user->employee->full_name;
+                    }
+
+                    if($user->hasRole(['commerce.owner', 'commerce.employee'])) {
+                        $userType = 'merchant';
+                        $fullName = $user->merchant->name . ' ' . $user->merchant->surname;
+                    }
+
+                    if($user->hasRole(['client'])) {
+                        $userType = 'client';
+                        $fullName = $user->client->name . ' ' . $user->client->surname;
+                    }
+
+                    return response()->json(
+                        [
+                            'status'    => 'success',
+                            'token'     =>  $token,
+                            'userRole'  =>  $user->getRoles()[0]->slug, // Posición 0 porque no se maneja Multi Roles
+                            'userType'  =>  $userType,
+                            'fullName'  =>  $fullName,
+                        ], 200)->header('Authorization', $token);
                 }
-                return response()->json(['error' => 'login_error'], 422);
+                return response()->json(['error' => 'El Correo Electrónico y/o la Contraseña no coinciden'], 422);
             }
             else {
-                return response()->json(['error' => 'login_error Usuario no tiene permiso de autenticarse'], 422);
+                return response()->json(['error' => 'Usuario no tiene permiso de autenticarse'], 422);
             }
         }
-        else return response()->json(['error' => 'login_error Usuario no existe'], 422);
+        else return response()->json(['error' => 'Usuario no existe'], 422);
     }
 
     public function loginCellphoneNumber(Request $request)
@@ -40,7 +81,7 @@ class AuthController extends Controller
         [
             'cellphone_number.string'   =>  'El Teléfono Celular tiene caracteres inválidos',
             'cellphone_number.between'  =>  'El Teléfono Celular debe contener entre 4 y 32 caracteres',
-            'cellphone_number.exists'   =>  'El Teléfono Celular y/o la contraseña no coinciden',
+            'cellphone_number.exists'   =>  'El Teléfono Celular y/o la Contraseña no coinciden',
             'password.string'           =>  'La Contraseña contiene caracteres inválidos',
             'password.between'          =>  'La Contraseña debe contener entre 8 y 255 caracteres',
         ]);
@@ -56,9 +97,33 @@ class AuthController extends Controller
             if($user->flag_login == true) {
                 $credentials = ['email'   =>  $user->email, 'password' => $request->password];
                 if ($token = $this->guard()->attempt($credentials)) {
-                    return response()->json(['status' => 'success', 'token' => $token], 200)->header('Authorization', $token);
+                    $userType;
+                    $fullName;
+                    if($user->hasRole(['ceo', 'cto', 'gabu.employee'])) {
+                        $userType = 'employee';
+                        $fullName = $user->employee->full_name;
+                    }
+
+                    if($user->hasRole(['commerce.owner', 'commerce.employee'])) {
+                        $userType = 'merchant';
+                        $fullName = $user->merchant->name . ' ' . $user->merchant->surname;
+                    }
+
+                    if($user->hasRole(['client'])) {
+                        $userType = 'client';
+                        $fullName = $user->client->name . ' ' . $user->client->surname;
+                    }
+
+                    return response()->json(
+                        [
+                            'status'    => 'success',
+                            'token'     =>  $token,
+                            'userRole'  =>  $user->getRoles()[0]->slug, // Posición 0 porque no se maneja Multi Roles
+                            'userType'  =>  $userType,
+                            'fullName'  =>  $fullName,
+                        ], 200)->header('Authorization', $token);
                 }
-                return response()->json(['error' => 'El Teléfono Celular y/o la contraseña no coinciden'], 422);
+                return response()->json(['error' => 'El Teléfono Celular y/o la Contraseña no coinciden'], 422);
             }
             else {
                 return response()->json(['error' => 'Usuario no tiene permiso de autenticarse'], 422);
